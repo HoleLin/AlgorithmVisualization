88662c28ef1379930f33938f9034ef8cf5f8a456
HoleLin <HoleLin@163.com>
2019-02-17 17:54:34 +0800

    Initial commit

---
+++ src/main/java/com/holelin/App.java
@@ -0,0 +1,13 @@
+package com.holelin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
